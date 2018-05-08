1a350a64596e4e63401ff7409f20f66c721b0ff0
Adrain007 <37847682+Adrain007@users.noreply.github.com>
2018-05-08 10:40:01 +0300

    Update Lexer.java

--- src/Lexer/Lexer.java
+++ src/Lexer/Lexer.java
@@ -6,13 +6,15 @@ import java.util.regex.Pattern;
 public class Lexer {
     static private StringBuffer currentString = new StringBuffer();
     static private StringBuffer acc = new StringBuffer();
+    static private n = 0;
 
-    public ArrayList<Token> parse(String string) {
+    public ArrayList<Token> parse(String string) throws Exeption{
         ArrayList<Token> tokenList = new ArrayList<>();
         while(string.length()!=0) {
             for(LexemType lexemType: LexemType.values()) {
                 Pattern pattern = lexemType.pattern;
                 Matcher matcher = pattern.matcher(string);
+                ++n;
                 if(matcher.lookingAt()){
                     currentString.append(matcher.group());
                     tokenList.add(new Token(lexemType.type,currentString.toString()));
@@ -22,6 +24,9 @@ public class Lexer {
                     currentString.setLength(0);
                     acc.setLength(0);
                     break;
+                }
+                else if (n == LexemType.values().length()){
+                    throw new Exeption("Ne no napisal, debik!!!");
                 }
             }
         }
